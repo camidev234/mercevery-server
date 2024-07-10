76f0c0bb161c261f80487c9cd4f0dd21c2fafecf
Camidev234 <camilo222andresace@gmail.com>
2024-07-09 23:17:52 -0500

    first migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
  
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject; 
@@ -54,6 +55,10 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTIdentifier()
     {
         return $this->getKey();
+    }
+
+    public function role() :BelongsTo {
+        return $this->belongsTo(Role::class);
     }
  
     /**
